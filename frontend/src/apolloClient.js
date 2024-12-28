c4307a940b6f757ede78ef7e59aaa1c38a8573fa
Kandekore <darren@kandekore.net>
2024-12-28 09:24:34 +0000

    Trigger rebuild with updated env

--- frontend/src/apolloClient.js
+++ frontend/src/apolloClient.js
@@ -1,7 +1,6 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
-// Use an environment variable if available, otherwise default to localhost
 const httpLink = createHttpLink({
   uri: process.env.REACT_APP_GRAPHQL_URI || 'http://localhost:4000/graphql',
 });
